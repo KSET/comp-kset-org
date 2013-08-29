5fccc375e29c5e55213c74f25ba875c3146bfc0e
Tomislav Maricevic <tmarice@gmail.com>
2013-08-29 15:26:58 +0200

    Changed static files folder name, added a new admin

--- comp/comp/settings/base.py
+++ comp/comp/settings/base.py
@@ -8,6 +8,7 @@ def ABS_PATH(*args):
     return os.path.join(ROOT_DIR, *args)
 
 ADMINS = (
+    ('Tomislav Maricevic', 'tmarice@kset.org'),
     ('Josip Grabic', 'grabich@kset.org'),
     ('Belma Gutlic', 'morrigan@kset.org'),
 )
@@ -43,7 +44,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = ABS_PATH('static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -51,7 +52,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    ABS_PATH('static'),
+    ABS_PATH('staticfiles'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
